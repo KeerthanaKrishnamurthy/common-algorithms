7955a9187b97180dc36843ab367fd8e8c6b214eb
KeerthanaKrishnamurthy <krishnamurthykeerthana99@gmail.com>
2025-04-17 12:13:53 -0400

    Modified Linear Search algorithm

--- src/main/java/org/example/LinearSearch.java
+++ src/main/java/org/example/LinearSearch.java
@@ -6,12 +6,26 @@ public class LinearSearch {
         int target = 5;
         System.out.println(isPresent(nums, target));
     }
-    private static boolean isPresent(int[] nums, int target){
-        for(int num:nums){
-            if(num == target){
-                return true;
+
+//  If you want to return the index/position of the number if present, else return -1
+
+    private static int isPresent(int[] nums, int target){
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target){
+                return i;
             }
         }
-        return false;
+        return -1;
     }
+
+//    If you want to check if the number is present or not
+
+//    private static boolean isPresent(int[] nums, int target){
+//        for(int num:nums){
+//            if(num == target){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
